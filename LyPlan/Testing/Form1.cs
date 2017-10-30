2d7c66d280e014f2b70503ad82954f4bec9a5407
Trung <damdinhductrung@gmail.com>
2017-10-30 10:55:36 +0700

    add data.sql

--- LyPlan/Testing/Form1.cs
+++ LyPlan/Testing/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,7 +18,30 @@ namespace Testing
         {
             InitializeComponent();
 
-            txtConn.Text = DataProvider.DataProvider.getConnectionString();
+            string strConnection = ConfigurationManager.ConnectionStrings["LyPlan"].ConnectionString;
+            string SQL = "select * from Task";
+            SqlConnection cnn = new SqlConnection(strConnection);
+            SqlCommand cmd = new SqlCommand(SQL, cnn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dtTask = new DataTable();
+            try
+            {
+                if (cnn.State == ConnectionState.Closed)
+                {
+                    cnn.Open();
+                }
+                da.Fill(dtTask);
+            }
+            catch (SqlException se)
+            {
+                throw new Exception(se.Message);
+            }
+            finally
+            {
+                cnn.Close();
+            }
+
+            dgvTask.DataSource = dtTask;
         }
     }
 }
