f090a1bbb449e3af270fa5da8dc54921d3797354
Trung <damdinhductrung@gmail.com>
2017-10-30 10:12:54 +0700

    Entities

---
+++ LyPlan/BackEnd/DataProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProvider
+{
+    public class DataProvider
+    {
+        public DataProvider()
+        {
+
+        }
+
+        public static string getConnectionString()
+        {
+            string strConnection = ConfigurationManager.ConnectionStrings["LyPlan"].ConnectionString;
+            return strConnection;
+        }
+    }
+}
--- LyPlan/BussinessObject/Class1.cs
+++ LyPlan/BussinessObject/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BackEnd
+namespace BussinessObject
 {
     public class Class1
     {
